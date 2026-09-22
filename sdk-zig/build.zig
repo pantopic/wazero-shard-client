4fd7c2a2020a6b89b4403ebe691ae6b04f883ca4
kevburnsjr <kevburnsjr@gmail.com>
2026-09-22 04:46:30 -0700

    simplify zig sdk build

--- sdk-zig/build.zig
+++ sdk-zig/build.zig
@@ -1,26 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const module = b.createModule(.{
+    _ = b.addModule("shard_client", .{
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
     });
-
-    const lib = b.addLibrary(.{
-        .name = "shard_client",
-        .root_module = module,
-    });
-    b.installArtifact(lib);
-
-    const tests = b.addTest(.{
-        .root_module = module,
-    });
-
-    const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run the Zig SDK tests");
-    test_step.dependOn(&run_tests.step);
 }
--- sdk-zig/src/root.zig
+++ sdk-zig/src/root.zig
@@ -2,21 +2,21 @@ pub const abi = @import("abi.zig");
 pub const errors = @import("errors.zig");
 pub const sdk = @import("sdk.zig");
 
-pub const Client = sdk.Client;
-pub const ReadResult = sdk.ReadResult;
 pub const ApplyResult = sdk.ApplyResult;
-pub const New = sdk.New;
-pub const RegisterStreamRecv = sdk.RegisterStreamRecv;
-pub const RegisterAsyncRecv = sdk.RegisterAsyncRecv;
-pub const HostError = sdk.HostError;
-pub const StreamRecvFn = abi.StreamRecvFn;
-pub const AsyncRecvFn = abi.AsyncRecvFn;
-pub const Error = errors.Error;
-pub const StreamRecvError = errors.StreamRecvError;
 pub const AsyncRecvError = errors.AsyncRecvError;
+pub const AsyncRecvFn = abi.AsyncRecvFn;
+pub const Client = sdk.Client;
+pub const ErrHost = errors.ErrHost;
+pub const Error = errors.Error;
 pub const ErrStreamRecvAlreadyRegistered = errors.ErrStreamRecvAlreadyRegistered;
 pub const ErrStreamRecvNotRegistered = errors.ErrStreamRecvNotRegistered;
-pub const ErrHost = errors.ErrHost;
+pub const HostError = sdk.HostError;
+pub const New = sdk.New;
+pub const ReadResult = sdk.ReadResult;
+pub const RegisterAsyncRecv = sdk.RegisterAsyncRecv;
+pub const RegisterStreamRecv = sdk.RegisterStreamRecv;
+pub const StreamRecvError = errors.StreamRecvError;
+pub const StreamRecvFn = abi.StreamRecvFn;
 
 test {
     _ = @import("sdk.zig");
